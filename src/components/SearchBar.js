814a3349f3423ab4a4a26f61e7a839bbab69d643
riccar <ricardocarballo@gmail.com>
2017-12-28 17:07:12 +1100

    Creating components file structure

---
+++ src/components/SearchBar.js
@@ -0,0 +1,15 @@
+//Class based Search Bar component
+
+//Curly brackets means import React, create a Component variable and initialize it with the React.Component property
+import React, { Component } from 'react';
+
+//Components variable is available and can be used as extender of the class
+class SearchBar extends Component {
+  
+  //Every React class based component must have a render method
+  render() {
+    return <input />;
+  }
+}
+
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
+//Import react so it can be used inside this module. The transpiler will take care of provide access to it. 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+import SearchBar from './components/SearchBar';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const API_KEY = 'AIzaSyAE1JpbAKurn0ZPMbD-d-TcXxoFP-Sjl3Y';
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+//Create a new component (a class) to produce some html 
+//The parenthesis and the fat arrow is the new ES6 syntax for function() keyword1
+const App = () => {
+  //jsx is a subset or dialect of javascript that allows us to write what looks like html tags but it's actually javascript
+  //we use jsx to produce the actual xml to insert into the DOM once it's transpiled into vanilla javascript . It's a lot easier 
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
+}
+//Put the component's generated html into the web page DOM 
+//React render the instance of the component App (<App / >).
+//To create instances of components, just wrap them betwee333333n JSX tags for the transpiler to translate itf into javascript React.createElement("app",null)
+ReactDOM.render(<App />, document.querySelector('.container'));
